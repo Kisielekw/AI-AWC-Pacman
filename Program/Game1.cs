06e20f78c534e24c3f688bc0ca4f501e0df76a50
Kisielek <d.kisielewski-2021@hull.ac.uk>
2022-11-14 14:59:49 +0000

    Drew a paackman ghost

--- Program/Game1.cs
+++ Program/Game1.cs
@@ -8,6 +8,7 @@ namespace Pacman
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private ShapeBatcher _shapeBatcher;
 
         public Game1()
         {
@@ -28,6 +29,8 @@ namespace Pacman
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            _shapeBatcher = new ShapeBatcher(GraphicsDevice);
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +48,20 @@ namespace Pacman
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _shapeBatcher.Begin();
+            
+            _shapeBatcher.DrawCircle(new Vector2(175, 250), 25, 16, Color.Red);
+            _shapeBatcher.DrawRectangle(new Vector2(150, 225), 25, 50, Color.Red);
+            _shapeBatcher.DrawCircle(new Vector2(155, 227), 5, 3, Color.CornflowerBlue);
+            _shapeBatcher.DrawCircle(new Vector2(165, 227), 5, 3, Color.CornflowerBlue);
+            _shapeBatcher.DrawCircle(new Vector2(175, 227), 5, 3, Color.CornflowerBlue);
+            _shapeBatcher.DrawCircle(new Vector2(185, 227), 5, 3, Color.CornflowerBlue);
+            _shapeBatcher.DrawCircle(new Vector2(195, 227), 5, 3, Color.CornflowerBlue);
+            _shapeBatcher.DrawCircle(new Vector2(165, 255), 7, 10, Color.White);
+            _shapeBatcher.DrawCircle(new Vector2(185, 255), 7, 10, Color.White);
+            _shapeBatcher.DrawCircle(new Vector2(170, 255), 3.5f, 10, Color.DarkBlue);
+            _shapeBatcher.DrawCircle(new Vector2(190, 255), 3.5f, 10, Color.DarkBlue);
+            _shapeBatcher.End();
 
             base.Draw(gameTime);
         }
