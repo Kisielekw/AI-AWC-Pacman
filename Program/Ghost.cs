c2c77c60f939cc998460568f4c942e81a51269d5
Kisielekw <dawkis1@outlook.com>
2023-01-09 18:58:31 +0000

    started implementation of enum state mechines

--- Program/Ghost.cs
+++ Program/Ghost.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Microsoft.Xna.Framework;
 
 namespace Pacman
 {
     internal abstract class Ghost
     {
+        protected enum States { House, Corner, Chase, Escape }
+
         public string Name { get; protected set; }
         public Vector2 Position { get; protected set; }
 
         protected Color colour;
         protected Color backgroundColour;
+        protected States state;
 
         public Ghost(Vector2 pPosition, Color pBackgroundColour)
         {
             Position = pPosition;
+            backgroundColour = pBackgroundColour;
+            state = States.House;
         }
 
         public void Draw(ShapeBatcher pShapeBatcher)
@@ -34,6 +40,30 @@ namespace Pacman
             pShapeBatcher.DrawCircle(new Vector2(-5, 17.5f) + Position, 3.5f, 10, Color.DarkBlue);
             pShapeBatcher.DrawCircle(new Vector2(15, 17.5f) + Position, 3.5f, 10, Color.DarkBlue);
         }
+
+        public void Update(Pacman pPacman, float pSeconds)
+        {
+            switch (state)
+            {
+                case States.House:
+                    House(pSeconds);
+                    break;
+                case States.Corner:
+                    Corner(pSeconds);
+                    break;
+                case States.Chase:
+                    Chase(pSeconds, pPacman);
+                    break;
+                case States.Escape:
+                    Escape(pSeconds, pPacman);
+                    break;
+            }
+    }
+
+        abstract protected void House(float pSeconds);
+        abstract protected void Corner(float pSeconds);
+        abstract protected void Chase(float pSeconds, Pacman pPacman);
+        abstract protected void Escape(float pSeconds, Pacman pPacman);
     }
 
     internal class Blinky : Ghost
@@ -42,6 +72,26 @@ namespace Pacman
         {
             Name = "Blinky";
             colour = Color.Red;
+        }
+
+        protected override void Chase(float pSeconds, Pacman pPacman)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Corner(float pSeconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Escape(float pSeconds, Pacman pPacman)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void House(float pSeconds)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -52,6 +102,26 @@ namespace Pacman
             Name = "Pinky";
             colour = Color.Pink;
         }
+
+        protected override void Chase(float pSeconds, Pacman pPacman)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Corner(float pSeconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Escape(float pSeconds, Pacman pPacman)
+        {
+            throw new NotImplementedException();
+    }
+
+        protected override void House(float pSeconds)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     internal class Inky : Ghost
@@ -61,6 +131,26 @@ namespace Pacman
             Name = "Inky";
             colour = Color.LightBlue;
         }
+
+        protected override void Chase(float pSeconds, Pacman pPacman)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Corner(float pSeconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Escape(float pSeconds, Pacman pPacman)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void House(float pSeconds)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     internal class Clyde : Ghost
@@ -69,6 +159,26 @@ namespace Pacman
         {
             Name = "Clyde";
             colour = Color.Orange;
+        }
+
+        protected override void Chase(float pSeconds, Pacman pPacman)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Corner(float pSeconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Escape(float pSeconds, Pacman pPacman)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void House(float pSeconds)
+        {
+            throw new NotImplementedException();
         }
     }
 }
