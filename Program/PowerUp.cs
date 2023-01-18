f0bf8d8ab323c396db51c6c4181e434ddc956d2a
Kisielekw <dawkis1@outlook.com>
2023-01-18 17:12:25 +0000

    created powerUps

---
+++ Program/PowerUp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    internal class PowerUp
+    {
+        public Vector2 Position;
+        
+        public PowerUp(Vector2 position)
+        {
+            Position = position;
+        }
+
+        public void Draw(ShapeBatcher pShapeBatcher)
+        {
+            pShapeBatcher.DrawCircle(Position, 12.5f, 10, Color.PeachPuff);
+        }
+    }
+}
